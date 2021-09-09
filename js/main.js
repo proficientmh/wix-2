e37539eee74fcf04b3886f87ebb44cf96f8bee70
proficientmh <87502934+proficientmh@users.noreply.github.com>
2021-09-09 22:41:22 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(window).load(funtion() {
+//     $(window).on('scroll', function() {
+//         if ($(window).scrollTop() > 0) {
+//             $('.header').addClass('sticky');
+//         } else {
+//             $('.header').removeClass('sticky');
+//         }
+//     })
+// })
