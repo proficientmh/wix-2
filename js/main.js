2bba8fa8f77b2b0863016601014c6341fc05dcde
proficientmh <87502934+proficientmh@users.noreply.github.com>
2021-09-10 12:33:00 +0600

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// $(window).load(funtion() {
-//     $(window).on('scroll', function() {
-//         if ($(window).scrollTop() > 0) {
-//             $('.header').addClass('sticky');
-//         } else {
-//             $('.header').removeClass('sticky');
-//         }
-//     })
-// })
